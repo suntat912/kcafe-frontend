f014552e6ec71337196ec7e884c52c838e3fb661
suntat912 <namanhtran09122004@gmail.com>
2026-06-25 09:02:18 +0700

    Use Render backend as default API URL

--- src/utils/userSession.js
+++ src/utils/userSession.js
@@ -1,5 +1,7 @@
-export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || 'http://127.0.0.1:5000';
+const rawApiBaseUrl =
+  process.env.REACT_APP_API_BASE_URL || 'https://kcafe-backend.onrender.com';
+
+export const API_BASE_URL = rawApiBaseUrl.replace(/\/$/, '');
 export const API_TIMEOUT = 15000;
 const USER_KEY = 'user';
 
